d6b93cc2c68087f0e8130f2117e358a6d6d036b0
Chris Shiplet <chris@nearengine.com>
2015-04-16 20:06:16 -0700

    Template tweaks

--- worker.py
+++ worker.py
@@ -8,8 +8,7 @@ printer.wake()
 printer.flush()
 printer.setDefault()
 
-printer.feed(2)
-
+printer.setLineHeight(20)
 printer.justify('C')
 printer.setSize('L')
 printer.inverseOn()
@@ -21,14 +20,18 @@ printer.setSize('M')
 
 printer.println('researchaz.org #141')
 
+printer.setLineHeight()
 printer.setSize('S')
-printer.setLineHeight(50)
 
 printer.println('Wed, 18 Feb 2015 11:16 pm MST')
 
+printer.feed(1)
 printer.justify('L')
 
 printer.println('Show Preview doesn\'t work without edit to body for existing blog post')
+
+printer.setLineHeight(40)
+
 printer.println('*** BUG')
 printer.println('*** HIGH PRIORITY')
 printer.println('*** MEDIUM COMPLEXITY')
