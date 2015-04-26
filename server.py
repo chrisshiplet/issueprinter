47c24291c5596ffc84d8d6dc635504913e4859d8
Chris Shiplet <chris@nearengine.com>
2015-04-25 20:57:15 -0700

    Fix relative path for db running from cron

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ from werkzeug.exceptions import HTTPException
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect('main.db')
+        db = g._database = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main.db'))
     return db
 
 # helper method for returning rows
