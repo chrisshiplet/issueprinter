1fc34265b6e80e26dd54c6bd02b5953fa0021323
Chris Shiplet <chris@nearengine.com>
2015-04-27 00:55:51 -0700

    Update beacon.py

--- beacon.py
+++ beacon.py
@@ -8,7 +8,7 @@ while True:
         printer = Adafruit_Thermal('/dev/ttyAMA0', 19200, timeout=5)
         pid = os.getpid()
         try:
-                ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close($
+                ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
         except:
                 ip = 'NO INTERFACES UP'
         output = '[%s] %s' % (pid, ip)
